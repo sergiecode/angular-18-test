d39577eedc9d18f574ae105b9b05f373a06c5b99
Sergie Code <sergiosnsdk@gmail.com>
2024-06-12 10:53:22 +0100

    home update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  menuOption: string = ''
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption
+  }
+}
